0198c3a16e1072e9c2b988ca49bb4df9bab451c8
kirstenr21 <82554708+kirstenr21@users.noreply.github.com>
2021-04-28 16:23:41 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent} from './WarningAlert/warningalert.component';
+import { SuccessalertComponent } from './successalert/successalert.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessalertComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
